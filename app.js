3048d808f245a395f2d25d65817b2f70c20cd77e
Gurman Singh <gurmandhanoa01@gmail.com>
2025-10-28 06:15:41 +0530

    server check api developed

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('My AI Doctor app is working fine!');
+});
+
 module.exports = app;
